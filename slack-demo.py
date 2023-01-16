de95be6a3eaba8a48a8c1c2cac037f3de6f17ede
Abraham Gonzalez <abe.gonzalez@berkeley.edu>
2023-01-16 11:36:58 -0800

    Free money

--- slack-demo.py
+++ slack-demo.py
@@ -7,11 +7,12 @@ import os
 import pathlib
 import subprocess
 
-retreat_dir = "/scratch/abe/winter-retreat-2023-firesim-gemmini-demo"
+retreat_dir = "/home/firetower/Documents"
 sd_scripts_dir = f"{retreat_dir}/slack-demo"
 g_scripts_dir = f"{sd_scripts_dir}"
 fdir = f"{retreat_dir}/firesim/"
 g_sw_dir = f"{fdir}/target-design/chipyard/generators/gemmini/software/gemmini-rocc-tests/"
+conda_torch = "/home/firetower/miniforge3/envs/torch"
 
 class SlackDemoApplication:
 
@@ -46,21 +47,27 @@ class SlackDemoApplication:
         channel = "winter-retreat-2023-firesim-demo"
         self.cid = None
 
+        post_channel = "winter-retreat-2023-firesim-demo-resp"
+        self.post_cid = None
+
+        self.old_hash = None
+
         channels = []
         # look for public channels only
         for page in self.client.conversations_list():
             channels = channels + page['channels']
 
         for c in channels:
-           if (c['name'] == channel):
-               self.cid = c['id']
-               break
+            if (c['name'] == channel):
+                self.cid = c['id']
+            if (c['name'] == post_channel):
+                self.post_cid = c['id']
 
-        if self.cid is None:
-            print("Could not find channel: " + channel)
+        if self.cid is None or self.post_cid is None:
+            print("Could not find channel: " + channel + " " + post_channel)
             exit(1)
         else:
-            print(f"Attaching to channel: {self.cid}")
+            print(f"Attaching to channel: {self.cid} {self.post_cid}")
 
     def do_it(self):
         print(f"Listing files from ts: {self.ts_from}")
@@ -91,7 +98,7 @@ class SlackDemoApplication:
                 break
 
         if len(results_group) > 0:
-            self.ts_from = float(results_group[-1]["ts"])
+            self.ts_from = float(results_group[0]["ts"])
 
             for message in results_group:
                 print("\n\n\n\n-------------------------------------")
@@ -109,12 +116,25 @@ class SlackDemoApplication:
         sorted_files = files
 
         if len(sorted_files) > 0:
-            x = sorted_files[0]
+
+            x = None
+
+            # TODO: Oh no!
+            for sf in sorted_files:
+                new_hash = hash(sf['url_private'])
+                if self.old_hash == new_hash:
+                    continue
+                self.old_hash = new_hash
+                x = sf
+                break
+
+            # x = sorted_files[0]
 
             print(f"Looking at file: {x['name']}")
 
             # get png preview associated w/ image (all images converted to this)
-            thumb_img_url = x[sorted([e for e in x.keys() if "thumb" in e])[0]]
+            #thumb_img_url = x[sorted([e for e in x.keys() if "thumb" in e])[0]]
+            thumb_img_url = x['url_private']
 
             img = requests.get(thumb_img_url, headers=self.auth_header)
             path = pathlib.PosixPath(os.path.join(self.img_dir, x['name'].replace(" ", "")))
@@ -123,6 +143,14 @@ class SlackDemoApplication:
                 f.write(img.content)
 
             print(f"Wrote file: {path}")
+
+            with open(path.resolve(), 'rb') as f:
+                self.client.files_upload_v2(
+                    channel=self.post_cid,
+                    file=f,
+                    filename=x['name'],
+                    initial_comment=f"MobileNet is predicting the following image...",
+                )
 
             # create the input file for the NN
             def run_and_fail(*args, **kwargs):
@@ -137,10 +165,10 @@ class SlackDemoApplication:
             run_and_fail(f"cp {path.resolve()} {g_scripts_dir}/audience_images/audience_images/image2.png", shell=True)
             run_and_fail(f"cp {path.resolve()} {g_scripts_dir}/audience_images/audience_images/image3.png", shell=True)
             run_and_fail(f"cp {path.resolve()} {g_scripts_dir}/audience_images/audience_images/image4.png", shell=True)
-            run_and_fail(f"source $(conda info --base)/etc/profile.d/conda.sh && conda activate /home/abe/miniforge3/envs/torch && cd {g_scripts_dir} && python3 gen_images.py", shell=True)
+            run_and_fail(f"source $(conda info --base)/etc/profile.d/conda.sh && conda activate {conda_torch} && cd {g_scripts_dir} && python3 gen_images.py", shell=True)
             run_and_fail(f"cp {g_scripts_dir}/images.h {g_sw_dir}/imagenet/images.h", shell=True)
             run_and_fail(f"cd {g_sw_dir} && ./build.sh imagenet", shell=True)
-            run_and_fail(f"cd {retreat_dir} && ./temp.sh", shell=True)
+            run_and_fail(f"cd {sd_scripts_dir} && ./run-fs.sh", shell=True)
             t = subprocess.run(f"grep -r -h 'P .: ' {fdir}/deploy/results-workload", shell=True, capture_output=True)
             if t.returncode != 0:
                 print("Unable to get predictions")
@@ -150,9 +178,22 @@ class SlackDemoApplication:
                 for pred in preds:
                     print(f"Reading prediction: {pred}")
                     prednum = int(pred.split()[-1])
-                    o = run_and_fail(f"source $(conda info --base)/etc/profile.d/conda.sh && conda activate /home/abe/miniforge3/envs/torch && cd {g_scripts_dir} && python3 print_class.py {prednum}", shell=True, capture_output=True)
+                    o = run_and_fail(f"source $(conda info --base)/etc/profile.d/conda.sh && conda activate {conda_torch} && cd {g_scripts_dir} && python3 print_class.py {prednum}", shell=True, capture_output=True)
                     labelname = str(o.stdout, 'UTF-8')
-                    self.client.chat_postMessage(channel=self.cid, text=f"IMG: `{x['name']}` is _100%, no question_... `{labelname.strip()}`!")
+
+                    # with open(path.resolve(), 'rb') as f:
+                    #     self.client.files_upload_v2(
+                    #         channel=self.post_cid,
+                    #         file=f,
+                    #         filename=x['name'],
+                    #         initial_comment=f"MobileNet predicted this is `{labelname.strip()}`!",
+                    #     )
+
+                    self.client.chat_postMessage(
+                        channel=self.post_cid,
+                        text=f"MobileNet predicted this is `{labelname.strip()}`!",
+                    )
+
 
                     # since all 4 predictions are the same immediatedly exit
                     break
@@ -160,6 +201,9 @@ class SlackDemoApplication:
 if __name__ == "__main__":
     app = SlackDemoApplication()
     while True:
-        print("Loopin'")
-        app.do_it()
-        time.sleep(5)
+        try:
+            print("Loopin'")
+            app.do_it()
+            time.sleep(5)
+        except:
+            pass
